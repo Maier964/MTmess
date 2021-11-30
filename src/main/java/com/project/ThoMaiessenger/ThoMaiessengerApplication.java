da9cc8b64adbeb85d4d34515232cca384705fb8e
Maier964 <andreimaier311@gmail.com>
2021-11-30 18:05:18 +0200

    Initial Commit

---
+++ src/main/java/com/project/ThoMaiessenger/ThoMaiessengerApplication.java
@@ -0,0 +1,13 @@
+package com.project.ThoMaiessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThoMaiessengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThoMaiessengerApplication.class, args);
+	}
+
+}
