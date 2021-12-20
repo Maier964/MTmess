f49b9da5c0c99533f50928cc95d9c92dd3e0f330
Maier964 <andreimaier311@gmail.com>
2021-12-20 15:20:38 +0200

    Implemented main DB backend controller

---
+++ src/main/java/com/project/MTmess/Controller/UserController.java
@@ -0,0 +1,26 @@
+package com.project.MTmess.Controller;
+
+
+import com.project.MTmess.Model.UserEntity;
+import com.project.MTmess.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/add")
+    public String add(@RequestBody UserEntity user){
+        try {
+            user.setHashed_password( user.getHash( user.getHashed_password() ) );
+            userService.saveUser(user);
+            return "User was added!";
+        }catch ( Exception e ) {
+            return "Failed to add user.";
+        }
+    }
+}
